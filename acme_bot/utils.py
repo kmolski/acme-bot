fc12f07b4ec71a1692e152e87c64bfe463a0fee5
kmolski <starfighter413@gmail.com>
2021-09-04 13:05:05 +0200

    Simplify message splitting, bump dependencies

--- acme_bot/utils.py
+++ acme_bot/utils.py
@@ -7,9 +7,7 @@ MAX_MESSAGE_LENGTH = 2000
 
 def split_message(text, limit):
     """Splits a long message into chunks that are limited in length."""
-    lines = chain.from_iterable(
-        wrap(line, limit) if len(line) > limit else [line] for line in text.splitlines()
-    )
+    lines = chain.from_iterable(wrap(line, limit) for line in text.splitlines())
 
     messages = []
     current_msg = ""
