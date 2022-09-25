60714de14a2c7e6d6741a977d429d562caec227e
kmolski <krzysztof.molski29@gmail.com>
2022-09-25 09:56:24 +0200

    Add ConfigProperty.get method with default, make RABBITMQ_URI optional

--- acme_bot/config/__init__.py
+++ acme_bot/config/__init__.py
@@ -1,37 +1,12 @@
-"""Configuration management"""
+"""Configuration management using environment variables and config files."""
 import logging
+from collections.abc import Callable
 from dataclasses import dataclass
 from os import environ
 from os.path import dirname, join
 from typing import Any
-from collections.abc import Callable
 
 from dotenv import load_dotenv
-
-
-@dataclass
-class ConfigProperty:
-    """Configuration property loaded from environment variables or config files"""
-
-    key: str
-    constructor: Callable[[str], Any]
-
-    def __call__(self):
-        try:
-            return self.constructor(environ[self.key])
-        except KeyError:
-            logging.critical(
-                "Required config property is missing: %s. "
-                "Please provide it using a config file or environment variable.",
-                self.key,
-            )
-            raise
-
-
-COMMAND_PREFIX = ConfigProperty("COMMAND_PREFIX", str)
-DISCORD_TOKEN = ConfigProperty("DISCORD_TOKEN", str)
-LOG_LEVEL = ConfigProperty("LOG_LEVEL", logging.getLevelName)
-RABBITMQ_URI = ConfigProperty("RABBITMQ_URI", str)
 
 
 def load_config(config_path=None):
@@ -39,3 +14,40 @@ def load_config(config_path=None):
     if config_path is not None:
         load_dotenv(config_path)
     load_dotenv(join(dirname(__file__), "default.conf"))
+
+
+@dataclass
+class ConfigProperty:
+    """Config property loaded from environment variables and config files."""
+
+    env_name: str
+    constructor: Callable[[str], Any]
+
+    def __call__(self):
+        try:
+            return self.constructor(environ[self.env_name])
+        except KeyError:
+            logging.critical(
+                "Required config property is missing: %s. "
+                "Please provide it using a config file or environment variable.",
+                self.env_name,
+            )
+            raise
+
+    def get(self, *, default=None):
+        """Return the property value, or `default` if the property is not specified."""
+        if env_value := environ.get(self.env_name):
+            return self.constructor(env_value)
+
+        return default
+
+
+# Credentials
+DISCORD_TOKEN = ConfigProperty("DISCORD_TOKEN", str)
+RABBITMQ_URI = ConfigProperty("RABBITMQ_URI", str)
+
+# Command subsystem
+COMMAND_PREFIX = ConfigProperty("COMMAND_PREFIX", str)
+
+# Logging subsystem
+LOG_LEVEL = ConfigProperty("LOG_LEVEL", logging.getLevelName)
