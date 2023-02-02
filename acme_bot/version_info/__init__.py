9a303f3d229527dc7d99a1e1ac7201b90d955652
kmolski <krzysztof.molski29@gmail.com>
2023-02-02 13:58:15 +0100

    Change !version command help text

--- acme_bot/version_info/__init__.py
+++ acme_bot/version_info/__init__.py
@@ -1,4 +1,19 @@
 """Version & license information commands."""
+#  Copyright (C) 2022-2023  Krzysztof Molski
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from dataclasses import dataclass
 from importlib.metadata import version
 from os.path import dirname, join
@@ -66,7 +81,7 @@ The source code of this build is available here: {}.
 
     @commands.command(aliases=["ver"])
     async def version(self, ctx):
-        """Print the bot version and a link to its source code."""
+        """Print the bot version and link to the source code of the current build."""
 
         if ctx.display:
             content = self.MESSAGE_TEMPLATE.format(
