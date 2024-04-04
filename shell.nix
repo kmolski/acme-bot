ce80b00b155993378383cab0b990e4c171b6d5d4
kmolski <krzysztof.molski29@gmail.com>
2024-04-04 18:49:04 +0200

    fix(dev): Add poetry-core package

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python311.withPackages (ps: with ps; [ poetry-dynamic-versioning ]))
+    (python311.withPackages (ps: with ps; [ poetry-core poetry-dynamic-versioning ]))
     poetry
   ];
 }
