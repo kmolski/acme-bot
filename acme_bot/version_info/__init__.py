7bd5342e5b33969e6ab4a14ed1ebbd840f8b63fa
kmolski <krzysztof.molski29@gmail.com>
2025-05-17 18:11:33 +0200

    chore(python): Upgrade minimum Python version to 3.12

--- acme_bot/version_info/__init__.py
+++ acme_bot/version_info/__init__.py
@@ -1,6 +1,6 @@
 """Version & license information commands."""
 
-#  Copyright (C) 2022-2023  Krzysztof Molski
+#  Copyright (C) 2022-2025  Krzysztof Molski
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU Affero General Public License as published by
@@ -58,7 +58,7 @@ class BuildInfo:
 class VersionInfoModule(commands.Cog, CogFactory):
     """Version & license information commands."""
 
-    COPYRIGHT_INFO = "\n".join(["Copyright (C) 2019-2024  Krzysztof Molski"])
+    COPYRIGHT_INFO = "\n".join(["Copyright (C) 2019-2025  Krzysztof Molski"])
 
     MESSAGE_TEMPLATE = """
 acme-bot {} ({} {})
