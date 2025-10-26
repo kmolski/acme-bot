6a8b6c7dda44d23629e5d08ef7c7cc3f74182a38
kmolski <krzysztof.molski29@gmail.com>
2025-10-26 20:35:34 +0100

    chore(python): Bump required Python version

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python3.withPackages (ps: with ps; [ python-lsp-server ]))
+    (python313.withPackages (ps: with ps; [ python-lsp-server ]))
     poetry
   ];
 }
